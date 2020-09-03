167f2078bf28ae2382c1ceea522dbbdca32cf9c8
António Dias <adias@mosano.eu>
2020-09-03 15:44:01 +0100

    Updates

---
+++ precache-manifest.c4c080c8532ff4d96440535d86117a7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f777cd77558c65f6cacb9b95c8fb8ae",
+    "url": "/type-oh/index.html"
+  },
+  {
+    "revision": "301bdf979ecae98cc8a0",
+    "url": "/type-oh/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "69614a302ff31da3ec49",
+    "url": "/type-oh/static/js/2.d18e8443.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/type-oh/static/js/2.d18e8443.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "301bdf979ecae98cc8a0",
+    "url": "/type-oh/static/js/main.15055fb1.chunk.js"
+  },
+  {
+    "revision": "d31d10bbf70ced4cc353",
+    "url": "/type-oh/static/js/runtime-main.9cf794b3.js"
+  }
+]);
