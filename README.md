8cfa226f07ab01149a070203f89f1e1dc260a751
António Dias <adias@mosano.eu>
2020-09-03 15:28:22 +0100

    Init repo. Basic app scaffolding

---
+++ README.md
@@ -0,0 +1,6 @@
+# Type-~~OH~~
+### yet another typing test
+
+Just a small off hours project, an attempt to build a quick, resposive, online typing test tool.
+
+Check out the [Demo](https://antonioxdias.github.io/type-oh/)!
---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App() {
+  return (
+    <main
+      style={{
+        display: 'grid',
+        placeItems: 'center',
+        backgroundColor: '#282c34',
+        height: '100vh',
+        fontSize: 'calc(10px + 2vmin)',
+        color: 'white'
+      }}
+    >
+      Type-OH
+    </main>
+  )
+}
+
+export default App
