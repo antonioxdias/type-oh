2709401690d63d4fd4f21bc6f9fca34fb117b3ac
António Dias <adias@mosano.eu>
2020-09-04 10:40:15 +0100

    Render individual chars. Color code to match the typed text.

--- src/components/Typer.tsx
+++ src/components/Typer.tsx
@@ -2,6 +2,12 @@ import React, { useRef, useState, Fragment } from 'react'
 
 const dummyTestText = 'bacon ipsum dolor amet hamburger pork chop tongue meatball turkey swine tenderloin ham bresaola capicola chislic ham hock biltong cupim jowl picanha brisket turducken pork chop filet'
 
+type CharState = 'yay' | 'nay' | 'empty'
+
+const yayColor = '#c7b798'
+const nayColor = '#f56c42'
+const emptyColor = '#808080'
+
 function Typer() {
   const inputRef = useRef<HTMLInputElement | null>(null)
   const [typedText, setTypedText] = useState('')
@@ -10,36 +16,55 @@ function Typer() {
     if (inputRef && inputRef.current) inputRef.current.focus()
   }
 
+  const charState = (index: number): CharState => {
+    if (typedText[index]) {
+      const isEq = typedText.charCodeAt(index) === dummyTestText.charCodeAt(index)
+      return isEq ? 'yay' : 'nay'
+    }
+    return 'empty'
+  }
+
+  const isSpaceChar = (char: string) => char.charCodeAt(0) === 32
+
+  const charColor = (state: CharState) => {
+    switch (state) {
+      case 'yay':
+        return yayColor
+      case 'nay':
+        return nayColor
+      default:
+        return emptyColor
+    }
+  }
+
   return (
     <Fragment>
       <div
-        onClick={ focusInput }
+        onClick={focusInput}
         style={{
-          position: 'relative',
           maxWidth: 720,
-          fontSize: 'calc(10px + 2vmin)'
+          fontSize: 'calc(10px + 2vmin)',
+          textAlign: 'center',
+          wordSpacing: 4
         }}
       >
-        <div
-          style={{
-            width: '100%',
-            color: '#808080'
-          }}
-        >
-          { dummyTestText }
-        </div>
-        <div
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            pointerEvents: 'none',
-            width: '100%',
-            color: '#c7b798'
-          }}
-        >
-          { typedText }
-        </div>
+        {dummyTestText.split('').map((char: string, index: number) => {
+          const state = charState(index)
+          const color = charColor(state)
+          const isNaySpace = isSpaceChar(char) && state === 'nay'
+          return (
+            <span
+              key={index}
+              style={{
+                color,
+                backgroundColor: isNaySpace ? nayColor : 'transparent',
+                borderRadius: 8
+              }}
+            >
+              {char}
+            </span>
+          )
+        })}
       </div>
       <input
         ref={inputRef}
